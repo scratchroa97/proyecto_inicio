6920cd46b6c41869e0154f2c66d1a6fb9e6a5591
tellezjm <jmtping@gmail.com>
2021-07-13 18:45:08 -0500

    somos 2

--- resources/views/somos.blade.php
+++ resources/views/somos.blade.php
@@ -7,14 +7,14 @@
 <div class="container">
     <div class="row">
       <div class="col-sm-12 d-flex justify-content-center">
-            <h1> Quienes Somos</h1>
+            <h1 > Quienes Somos</h1>
         </div>
-        <div class="col-sm-12 d-flex justify-content-center">
+        <div class="col-sm-12 d-flex justify-content-center ">
             <div class="col bg-primary text-white">
             A
             </div>
-            <div class="col bg-dark text-white border border-warning" >
-                <img src="https://www.celularizado.com/wp-content/uploads/2020/08/300-nombres-originales-para-grupos-de-WhatsApp-amigos-revisando-sus-celulares.png" alt="Grupo de bersonas con celulares">
+            <div class="col-6 bg-dark border border-warning p-5 d-flex justify-content-center"  >
+                <img src="https://www.celularizado.com/wp-content/uploads/2020/08/300-nombres-originales-para-grupos-de-WhatsApp-amigos-revisando-sus-celulares.png" alt="Grupo de bersonas con celulares" class="rounded">
             </div>
         </div>
       <div class="w-90">
