83683038f107a452c1b7ab5c1b36740b2b9b8ec0
tellezjm <jmtping@gmail.com>
2021-07-13 19:25:14 -0500

    somos 3

--- resources/views/somos.blade.php
+++ resources/views/somos.blade.php
@@ -6,14 +6,20 @@
 
 <div class="container">
     <div class="row">
-      <div class="col-sm-12 d-flex justify-content-center">
+      <div class="col-sm-12 d-flex justify-content-center text-primary p-4 font-size:20px" >
             <h1 > Quienes Somos</h1>
         </div>
-        <div class="col-sm-12 d-flex justify-content-center ">
-            <div class="col bg-primary text-white">
-            A
+        <div class="col-sm-12 d-flex justify-content-center font-weight-bold">
+            <div class="col text-body p-4 text-justify" >
+                Somos una empresa especializada en la comercialización
+                y distribución de celulares y accesorios, la cual pretende 
+                consolidarse en el mercado nacional como una de las compañías 
+                del sector telecomunicaciones con más experiencia, criterio técnico 
+                y precios competitivos en el portafolio existente. Es así como se ve 
+                fortalecida la relación comercial que convierte los retos en 
+                oportunidades y soluciones.
             </div>
-            <div class="col-6 bg-dark border border-warning p-5 d-flex justify-content-center"  >
+            <div class="col-6 bg-light border border-warning p-5 d-flex justify-content-center rounded"  >
                 <img src="https://www.celularizado.com/wp-content/uploads/2020/08/300-nombres-originales-para-grupos-de-WhatsApp-amigos-revisando-sus-celulares.png" alt="Grupo de bersonas con celulares" class="rounded">
             </div>
         </div>
