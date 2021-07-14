3e819f6a6862eb8f963dcf894b837ae5ed61e147
tellezjm <jmtping@gmail.com>
2021-07-13 19:44:17 -0500

    somos 3

--- resources/views/somos.blade.php
+++ resources/views/somos.blade.php
@@ -10,7 +10,7 @@
             <h1 > Quienes Somos</h1>
         </div>
         <div class="col-sm-12 d-flex justify-content-center font-weight-bold">
-            <div class="col text-body p-4 text-justify" >
+            <div class="col text-body p-5 text-justify" >
                 Somos una empresa especializada en la comercialización
                 y distribución de celulares y accesorios, la cual pretende 
                 consolidarse en el mercado nacional como una de las compañías 
@@ -22,7 +22,51 @@
             <div class="col-6 bg-light border border-warning p-5 d-flex justify-content-center rounded"  >
                 <img src="https://www.celularizado.com/wp-content/uploads/2020/08/300-nombres-originales-para-grupos-de-WhatsApp-amigos-revisando-sus-celulares.png" alt="Grupo de bersonas con celulares" class="rounded">
             </div>
-        </div>
+            </div>
+            <div class="col-12 d-flex justify-content-center text-primary p-4 font-size:20px" >
+                <h1 > ¿Qué nos diferencia?</h1>
+            </div>
+            <div class="container">
+                <div id="mk-icon-box-34" class="mk-box-icon-2  jupiter-donut- box-align-left ">
+                    <div class="mk-box-icon-2-image" style="width:48px;"> 
+                        <img src="https://vzlatam.com/wp-content/uploads/2019/05/precios.svg" alt="Precios competitivos y convenios personalizados"> 
+                    </div>
+                        <h3 class="mk-box-icon-2-title">Precios competitivos y convenios personalizados</h3>   
+                        <p class="mk-box-icon-2-content">Nuestro principal objetivo el ofrecer los mejores precios
+                            del mercado para generar mayores márgenes a nuestros clientes y así hacerlos nuestros aliados
+                            estratégicos.
+                        </p>
+                    </div>
+                    <div id="mk-icon-box-36" class="mk-box-icon-2  jupiter-donut- box-align-left ">
+                    <div class="mk-box-icon-2-image" style="width:48px;">        
+                        <img src="https://vzlatam.com/wp-content/uploads/2019/05/soporte.svg" alt="Acompañamiento y soporte"> 
+                    </div> 
+                        <h3 class="mk-box-icon-2-title">Acompañamiento y soporte</h3>
+                        <p class="mk-box-icon-2-content">El servicio es nuestro emblema. Nos esforzamos para desarrollar
+                            relaciones a largo plazo y no de tan solo una compra.
+                        </p>
+                    </div>
+                    <div id="mk-icon-box-38" class="mk-box-icon-2  jupiter-donut- box-align-left ">
+                    <div class="mk-box-icon-2-image" style="width:48px;">
+                        <img src="https://vzlatam.com/wp-content/uploads/2019/05/productos-1.svg" alt="Amplio portafolio de productos">
+                    </div>    
+                        <h3 class="mk-box-icon-2-title">Amplio portafolio de productos</h3>    
+                        <p class="mk-box-icon-2-content">Distribuimos las mejores marcas a nivel mundial, en diferentes referencias 
+                            y modelos. Si algún producto no se encuesta en nuestro portafolio, también brindamos el servicio de búsqueda 
+                            en el exterior.
+                        </p>
+                    </div>
+                    <div id="mk-icon-box-40" class="mk-box-icon-2  jupiter-donut- box-align-left ">
+                    <div class="mk-box-icon-2-image" style="width:48px;">
+                        <img src="https://vzlatam.com/wp-content/uploads/2019/05/legal.svg" alt="Documentación legal">
+                    </div>    
+                        <h3 class="mk-box-icon-2-title">Documentación legal</h3>    
+                        <p class="mk-box-icon-2-content">Todos nuestros productos cuentan con manifiestos de importación, 
+                            certificados de homologación y factura legal.
+                        </p>
+                    </div>
+            </div>
+            
       <div class="w-90">
       </div>
     </div>
