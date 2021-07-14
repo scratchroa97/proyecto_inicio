7b2fa959bdc2e2ba9884289826fa7e1aa11900fd
tellezjm <jmtping@gmail.com>
2021-07-13 19:52:46 -0500

    somos 4

--- resources/views/somos.blade.php
+++ resources/views/somos.blade.php
@@ -26,42 +26,42 @@
             <div class="col-12 d-flex justify-content-center text-primary p-4 font-size:20px" >
                 <h1 > ¿Qué nos diferencia?</h1>
             </div>
-            <div class="container">
-                <div id="mk-icon-box-34" class="mk-box-icon-2  jupiter-donut- box-align-left ">
-                    <div class="mk-box-icon-2-image" style="width:48px;"> 
-                        <img src="https://vzlatam.com/wp-content/uploads/2019/05/precios.svg" alt="Precios competitivos y convenios personalizados"> 
+            <div class="">
+                    <div id="mk-icon-box-34" class="mk-box-icon-2  jupiter-donut- box-align-left col-6">
+                        <div class="mk-box-icon-2-image" style="width:48px;"> 
+                            <img src="https://vzlatam.com/wp-content/uploads/2019/05/precios.svg" alt="Precios competitivos y convenios personalizados"> 
+                        </div>
+                            <h3 class="mk-box-icon-2-title">Precios competitivos y convenios personalizados</h3>   
+                            <p class="mk-box-icon-2-content text-justify">Nuestro principal objetivo el ofrecer los mejores precios
+                                del mercado para generar mayores márgenes a nuestros clientes y así hacerlos nuestros aliados
+                                estratégicos.
+                            </p>
                     </div>
-                        <h3 class="mk-box-icon-2-title">Precios competitivos y convenios personalizados</h3>   
-                        <p class="mk-box-icon-2-content">Nuestro principal objetivo el ofrecer los mejores precios
-                            del mercado para generar mayores márgenes a nuestros clientes y así hacerlos nuestros aliados
-                            estratégicos.
-                        </p>
+                    <div id="mk-icon-box-36" class="mk-box-icon-2  jupiter-donut- box-align-left col-6">
+                        <div class="mk-box-icon-2-image" style="width:48px;">        
+                            <img src="https://vzlatam.com/wp-content/uploads/2019/05/soporte.svg" alt="Acompañamiento y soporte"> 
+                        </div> 
+                            <h3 class="mk-box-icon-2-title">Acompañamiento y soporte</h3>
+                            <p class="mk-box-icon-2-content text-justify">El servicio es nuestro emblema. Nos esforzamos para desarrollar
+                                relaciones a largo plazo y no de tan solo una compra.
+                            </p>
                     </div>
-                    <div id="mk-icon-box-36" class="mk-box-icon-2  jupiter-donut- box-align-left ">
-                    <div class="mk-box-icon-2-image" style="width:48px;">        
-                        <img src="https://vzlatam.com/wp-content/uploads/2019/05/soporte.svg" alt="Acompañamiento y soporte"> 
-                    </div> 
-                        <h3 class="mk-box-icon-2-title">Acompañamiento y soporte</h3>
-                        <p class="mk-box-icon-2-content">El servicio es nuestro emblema. Nos esforzamos para desarrollar
-                            relaciones a largo plazo y no de tan solo una compra.
-                        </p>
-                    </div>
-                    <div id="mk-icon-box-38" class="mk-box-icon-2  jupiter-donut- box-align-left ">
+                    <div id="mk-icon-box-38" class="mk-box-icon-2  jupiter-donut- box-align-left col-6">
                     <div class="mk-box-icon-2-image" style="width:48px;">
                         <img src="https://vzlatam.com/wp-content/uploads/2019/05/productos-1.svg" alt="Amplio portafolio de productos">
                     </div>    
                         <h3 class="mk-box-icon-2-title">Amplio portafolio de productos</h3>    
-                        <p class="mk-box-icon-2-content">Distribuimos las mejores marcas a nivel mundial, en diferentes referencias 
+                        <p class="mk-box-icon-2-content text-justify">Distribuimos las mejores marcas a nivel mundial, en diferentes referencias 
                             y modelos. Si algún producto no se encuesta en nuestro portafolio, también brindamos el servicio de búsqueda 
                             en el exterior.
                         </p>
                     </div>
-                    <div id="mk-icon-box-40" class="mk-box-icon-2  jupiter-donut- box-align-left ">
+                    <div id="mk-icon-box-40" class="mk-box-icon-2  jupiter-donut- box-align-left col-6">
                     <div class="mk-box-icon-2-image" style="width:48px;">
                         <img src="https://vzlatam.com/wp-content/uploads/2019/05/legal.svg" alt="Documentación legal">
                     </div>    
                         <h3 class="mk-box-icon-2-title">Documentación legal</h3>    
-                        <p class="mk-box-icon-2-content">Todos nuestros productos cuentan con manifiestos de importación, 
+                        <p class="mk-box-icon-2-content text-justify">Todos nuestros productos cuentan con manifiestos de importación, 
                             certificados de homologación y factura legal.
                         </p>
                     </div>
