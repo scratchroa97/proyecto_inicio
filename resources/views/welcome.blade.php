9e96b831485e2914b419fb58e39e30e9d70aa94d
Andres Felipe Fonseca Mosquera <andrespipefon@hotmail.com>
2021-07-13 21:20:01 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,41 +3,40 @@
 
 
 <!-- Carrusel -->
-<div class="row">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
 
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active" style="height: 500px;">
-                <img src="https://pbs.twimg.com/media/EEHMp2DU8AIsuTe.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5 style="color: rgb(136, 136, 136)">Accesorios</h5>
-                    <p style="color: rgb(136, 136, 136)">Conoce todos nuestra linea de accesorios para tu móvil.
-                    </p>
-                </div>
-            </div>
-            <div class="carousel-item" style="height: 500px">
-                <img src="https://surflorida.com/wp-content/uploads/2021/02/mejores-celulares-2018.jpg"
-                    class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Celulares</h5>
-                    <p>Conoce los diferentes móviles al mejor precio del mercado.</p>
-                </div>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active" style="height: 500px;">
+            <img src="https://pbs.twimg.com/media/EEHMp2DU8AIsuTe.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5 style="color: rgb(136, 136, 136)">Accesorios</h5>
+                <p style="color: rgb(136, 136, 136)">Conoce todos nuestra linea de accesorios para tu móvil.
+                </p>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+        <div class="carousel-item" style="height: 500px">
+            <img src="https://surflorida.com/wp-content/uploads/2021/02/mejores-celulares-2018.jpg"
+                class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Celulares</h5>
+                <p>Conoce los diferentes móviles al mejor precio del mercado.</p>
+            </div>
+        </div>
     </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
+
 <!-- End Carrusel -->
 
 
