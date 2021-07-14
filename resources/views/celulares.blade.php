10ff3026868fcc3b2eba8b9b8d2e9cd795e5814f
Andres Felipe Fonseca Mosquera <andrespipefon@hotmail.com>
2021-07-13 20:45:54 -0500

    Update celulares.blade.php
    Se actualizó posible vista de celulares

--- resources/views/celulares.blade.php
+++ resources/views/celulares.blade.php
@@ -1,12 +1,46 @@
 @extends('layouts.pages')
 
 @section('content')
-<div class="jumbotron col-md-12">
+<!-- <div class="jumbotron col-md-12 text-white"
+    style="background-image: url(https://surflorida.com/wp-content/uploads/2021/02/mejores-celulares-2018.jpg); background-size: 100% 100%; opacity: 0.8;">
     <h1 class="text-center">Celulares</h1>
     <p class="text-center">Aquí encontrarás los celulares que tenemos disponibles</p>
+</div> -->
+<!-- Carrusel -->
+
+<div id="carouselExampleCaptions" class="carousel slide pb-5" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active" style="height: 300px">
+            <img src="https://surflorida.com/wp-content/uploads/2021/02/mejores-celulares-2018.jpg"
+                class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Celulares</h5>
+                <p>Conoce los diferentes móviles al mejor precio del mercado.</p>
+            </div>
+        </div>
+    </div>
 </div>
 
+<!-- End Carrusel -->
 
+<div class="col-md-12 pb-5">
+    <form>
+        <div class="form-row">
+            <div class="col">
+                <select type="text" class="form-control">
+                    <option value="">Ordenar por precio</option>
+                    <option value="">Mas barato a más costoso</option>
+                    <option value="">Más costoso a más barato</option>
+                </select>
+            </div>
+            <div class="col">
+                <select type="text" class="form-control">
+                    <option value="">Filtrar por marca</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</div>
 @foreach($phones as $phone)
 <div class="card col-sm-12 col-md-4 col-lg-3">
     <img src="{{asset('uploads/phones/'.$phone->foto)}}" class="card-img-top" alt="Se supone que va la imagen"
@@ -17,5 +51,6 @@
         <a href="#" class="btn btn-primary">¡Lo quiero!</a>
     </div>
 </div>
+
 @endforeach
 @endsection
